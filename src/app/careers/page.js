1e6ca82d93ef6971d4b21e1d14984fb7f90734de
0xshivam <shivamsharmashiv@outlook.com>
2025-09-01 12:51:56 +0530

    added career in app router for open jobs component

---
+++ src/app/careers/page.js
@@ -0,0 +1,88 @@
+
+export default function OpenRoles() {
+  return (
+    <div>
+      <div>
+        <h1>Open Roles</h1>
+        <p>
+          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
+          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
+          dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+        </p>
+      </div>
+      
+      <table>
+        <thead>
+          <tr>
+            <th>POSITION:</th>
+            <th>DEPARTMENT:</th>
+            <th>TYPE:</th>
+            <th>LOCATION:</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+          <tr>
+            <td>Account Executive, Enterprise</td>
+            <td>Sales</td>
+            <td>Remote</td>
+            <td>New York, United States</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}

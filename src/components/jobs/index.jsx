3503265e6a3f4d31cb48fb70665dd9f4708298c8
0xshivam <shivamsharmashiv@outlook.com>
2025-09-01 17:04:22 +0530

    added meaningful markup

---
+++ src/components/jobs/index.jsx
@@ -0,0 +1,108 @@
+export default function Jobs() {
+  return (
+    <section>
+      <div>
+        <h2>Open Roles</h2>
+        <p>
+          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
+          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
+          dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+        </p>
+      </div>
+      
+      <div>
+        {/* Header row */}
+        <div>
+          <span>POSITION:</span>
+          <span>DEPARTMENT:</span>
+          <span>TYPE:</span>
+          <span>LOCATION:</span>
+        </div>
+        
+        {/* Job listings */}
+        <ul>
+          <li>
+            <a href="/jobs/account-executive-1">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-2">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-3">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-4">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-5">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-6">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-7">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-8">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-9">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+          <li>
+            <a href="/jobs/account-executive-10">
+              <span>Account Executive, Enterprise</span>
+              <span>Sales</span>
+              <span>Remote</span>
+              <span>New York, United States</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}

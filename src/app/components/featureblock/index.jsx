0e2107db56614807baed65de0a3af75fb8adb281
0xshivam <shivamsharmashiv@outlook.com>
2025-09-02 16:36:10 +0530

    fix: file setup for new feature

---
+++ src/app/components/featureblock/index.jsx
@@ -0,0 +1,7 @@
+import styles from './featureblock.module.css'
+
+export default function Feature() {
+    return (
+    <h2>hello feature</h2>
+  );
+}
---
+++ src/app/feature/page.js
@@ -0,0 +1,10 @@
+import Feature from '../components/featureblock';
+
+export default function featureBlock() {
+ //data fetching here
+  return (
+    <main>
+      <Feature />
+    </main>
+  );
+}

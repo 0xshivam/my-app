fb88c24cea00cfdb97a02d6c5842bad64b1fd04e
0xshivam <shivamsharmashiv@outlook.com>
2025-09-02 11:05:53 +0530

    fix: added style, full page

--- src/app/components/jobs/index.jsx
+++ src/app/components/jobs/index.jsx
@@ -12,74 +12,99 @@ export default function Jobs() {
         </p>
       </div>
       
-      <div>
-        <div>
-          <span>POSITION:</span>
-          <span>DEPARTMENT:</span>
-          <span>TYPE:</span>
-          <span>LOCATION:</span>
+      
+      <div className={styles.tableContainer}>
+        {/* Header row */}
+        <div className={styles.headerRow}>
+          <span className={styles.headerCell}>Position</span>
+          <span className={styles.headerCell}>Department</span>
+          <span className={styles.headerCell}>Type</span>
+          <span className={styles.headerCell}>Location</span>
         </div>
         
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
-        <div>
-          <span>Account Executive, Enterprise</span>
-          <span>Sales</span>
-          <span>Remote</span>
-          <span>New York, United States</span>
-        </div>
+        {/* Job listings */}
+        <ul className={styles.jobsList}>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-1" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-2" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-3" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-4" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-5" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-6" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-7" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-8" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-9" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+          <li className={styles.jobItem}>
+            <a href="/jobs/account-executive-10" className={styles.jobLink}>
+              <span className={styles.jobCell}>Account Executive, Enterprise</span>
+              <span className={styles.jobCell}>Sales</span>
+              <span className={styles.jobCell}>Remote</span>
+              <span className={styles.jobCell}>New York, United States</span>
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   );

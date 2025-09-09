cc2ef2b573dac5da0d418664d30d9cbeffba2a08
0xshivam <shivamsharmashiv@outlook.com>
2025-09-09 08:12:25 +0530

    added hover effect on jobs page

--- src/app/components/jobs/index.jsx
+++ src/app/components/jobs/index.jsx
@@ -1,140 +1,134 @@
-import cn from 'clsx'
-import styles from './jobs.module.css'
+import cn from "clsx";
+import styles from "./jobs.module.css";
 
 export default function Jobs({ view }) {
   return (
     <section className={styles.container}>
       <div className={styles.header}>
-      {true && (
-        <h2 className={styles.title}>Open Roles</h2>
-      )}
-        <p className={cn(
-            view === 'grid' ? styles.description : styles.description2
-          )}>
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem 
-          ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum 
-          
+        {true && <h2 className={styles.title}>Open Roles</h2>}
+        <p
+          className={cn(
+            view === "grid" ? styles.description : styles.description2
+          )}
+        >
+          Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet lorem
         </p>
       </div>
-      
+
       <div className={styles.tableWrapper}>
-      <div className={styles.tableContainer}>
-        {/* Header row */}
-        <div className={styles.headerRow}>
-          <span className={styles.headerCell}>Position</span>
-          <span className={styles.headerCell}>Department</span>
-          <span className={styles.headerCell}>Type</span>
-          <span className={styles.headerCell}>Location</span>
+        <div className={styles.tableContainer}>
+          {/* Header row */}
+          <div className={styles.headerRow}>
+            <span className={styles.headerCell}>Position</span>
+            <span className={styles.headerCell}>Department</span>
+            <span className={styles.headerCell}>Type</span>
+            <span className={styles.headerCell}>Location</span>
+          </div>
+
+          {/* Job listings */}
+          <ul className={styles.jobsList}>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-1" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-2" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-3" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-4" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-5" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-6" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-7" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-8" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-9" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+            <li className={styles.jobItem}>
+              <a href="/jobs/account-executive-10" className={styles.jobLink}>
+                <span className={styles.jobCell}>
+                  Account Executive, Enterprise
+                </span>
+                <span className={styles.jobCell}>Sales</span>
+                <span className={styles.jobCell}>Remote</span>
+                <span className={styles.jobCell}>New York, United States</span>
+              </a>
+            </li>
+          </ul>
         </div>
-        
-        {/* Job listings */}
-        <ul className={styles.jobsList}>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-1" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-2" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-3" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-4" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-5" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-6" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-7" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-8" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-9" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-          <li className={styles.jobItem}>
-            <a href="/jobs/account-executive-10" className={styles.jobLink}>
-              <span className={styles.jobCell}>Account Executive, Enterprise</span>
-              <span className={styles.jobCell}>Sales</span>
-              <span className={styles.jobCell}>Remote</span>
-              <span className={styles.jobCell}>New York, United States</span>
-            </a>
-          </li>
-        </ul>
-      </div>
       </div>
     </section>
   );
--- src/app/testing/page.js
+++
@@ -1,7 +0,0 @@
-export default function Testing() {
-  return (
-    <main>
-      <h1>This is the Testing Page</h1>
-    </main>
-  );
-}

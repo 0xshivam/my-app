9fa2787b8ce6e899bee124dc87f890bfa63d7318
0xshivam <shivamsharmashiv@outlook.com>
2025-09-15 15:01:06 +0530

    storyblok

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+    https: {
+      key: './cert/localhost+2-key.pem',
+      cert: './cert/localhost+2.pem',
+    },
+  },
+};
+
 
 export default nextConfig;
